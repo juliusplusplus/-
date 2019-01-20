efe10f7feea6735635748f49f6ffe4624e9330cc
juliusplusplus <2985869424@qq.com>
2019-01-21 00:44:37 +0800

    exit问题修改原稿

--- 树状文件系统/paste.cpp
+++ 树状文件系统/paste.cpp
@@ -3,118 +3,120 @@
 
 int FileSystem::pasteFile() //粘贴文件
 {
-    MyFile *h = currentDir->filePtr;
+	MyFile *h = currentDir->filePtr;
 	MyFile *pTemp = h;
-    if (copytempfile == NULL) 
+	if (copytempfile == NULL)
 	{
-        cout << "NO FILE HAS BEEN COPIED          --FAILED" << endl;
-        return 0;
-    }
-    /*如果当前目录没有文件*/
-	if (disk_empty < copytempfile->size) 
-		{
-            cout << "NO ENOUGH SPACE            --FAILED";
-            return 0;
-        }
-    if (h == NULL) 
+		cout << "NO FILE HAS BEEN COPIED          --FAILED" << endl;
+		return 0;
+	}
+	/*如果当前目录没有文件*/
+	if (disk_empty < copytempfile->size)
 	{
-        
-        currentDir->filePtr = copy_file(copytempfile);
-        currentDir->size += copytempfile->size;
-		cout<<"THE FILE HAS BEEN PASTED          --SUCCEED" << endl;
-    }
-	else 
+		cout << "NO ENOUGH SPACE            --FAILED";
+		return 0;
+	}
+	if (h == NULL)
 	{
-        while (h != NULL) 
-		{
-            if (!strcmp(h->name, copytempfile->name)) 
-			{
-                cout << "FILE HAS EXISTED        --FAILED" << endl;
-                return 0;
-            }
-            break;
-            h = h->nextFile;
-        }
 
-        if (disk_empty < copytempfile->size) 
+		currentDir->filePtr = copy_file(copytempfile);
+		currentDir->size += copytempfile->size;
+		cout << "THE FILE HAS BEEN PASTED          --SUCCEED" << endl;
+	}
+	else
+	{
+		while (h != NULL)
 		{
-            cout << "NO ENOUGH SPACE            --FAILED";
-            return 0;
-        }
-        currentDir->filePtr = copy_file(copytempfile);
+			if (!strcmp(h->name, copytempfile->name))
+			{
+				cout << "FILE HAS EXISTED        --FAILED" << endl;
+				return 0;
+			}
+			break;
+			h = h->nextFile;
+		}
+
+		if (disk_empty < copytempfile->size)
+		{
+			cout << "NO ENOUGH SPACE            --FAILED";
+			return 0;
+		}
+		currentDir->filePtr = copy_file(copytempfile);
 		currentDir->filePtr->nextFile = pTemp;
-        currentDir->filePtr->nextFile = h;
-        currentDir->size += copytempfile->size;
-		cout<<"THE FILE HAS BEEN PASTED          --SUCCEED" << endl;
-    return 1;
-    }
-return 0;
-   
+		currentDir->filePtr->nextFile = h;
+		currentDir->size += copytempfile->size;
+		cout << "THE FILE HAS BEEN PASTED          --SUCCEED" << endl;
+		return 1;
+	}
+	return 0;
+
 }
 int FileSystem::pasteDir()    //粘贴目录
 {
-    MyDir *h, *d1;
-    h = currentDir->dirPtr;
-                                                                                       
-    if (copytempdir == NULL) 
+	MyDir *h, *d1;
+	h = currentDir->dirPtr;
+
+	if (copytempdir == NULL)
 	{
-        cout << "NO SUCH DIR HAS BEEN COPIED        --FAILED" << endl;
-        return 0;
-    }
-	if (disk_empty < copytempdir->size) 
-	{
-        cout << "NO ENOUGH SPACE     --FAILED";
-        return 0;
-    }
-	while(1)
-	{
-	
-	if(h=copytempdir->dirPtr)
-	{	cout<<"THIS IS FATHER DIR,CAN'T PASTE        --FAILED"<<endl;
-	return 0;}
-	if(h=root->dirPtr)
-		break;
-	h=h->preDir;
+		cout << "NO SUCH DIR HAS BEEN COPIED        --FAILED" << endl;
+		return 0;
 	}
-    if (h == NULL) //是否有子目录,若无
+	if (disk_empty < copytempdir->size)
 	{
-        
-        disk_empty -= copytempdir->size;    //减去大小
-        d1 = copy_dir(copytempdir);        //加入链表
-        currentDir->dirPtr = d1;
-        d1->preDir = currentDir;
-		MyDir *df=currentDir;
+		cout << "NO ENOUGH SPACE     --FAILED";
+		return 0;
+	}
+	while (1)
+	{
+
+		if (h = copytempdir->dirPtr)
+		{
+			cout << "THIS IS FATHER DIR,CAN'T PASTE        --FAILED" << endl;
+			return 0;
+		}
+		if (h = root->dirPtr)
+			break;
+		h = h->preDir;
+	}
+	if (h == NULL) //是否有子目录,若无
+	{
+
+		disk_empty -= copytempdir->size;    //减去大小
+		d1 = copy_dir(copytempdir);        //加入链表
+		currentDir->dirPtr = d1;
+		d1->preDir = currentDir;
+		MyDir *df = currentDir;
 		//更新父母目录大小
-		while(df!=NULL)
-		{	
-			df->size+=copytempdir->size;
-			df=df->preDir;
+		while (df != NULL)
+		{
+			df->size += copytempdir->size;
+			df = df->preDir;
 		}
 		delete df;
 
 
-    } 
-	else 
+	}
+	else
 	{
-        while (h != NULL) 
+		while (h != NULL)
 		{
-            if (!strcmp(h->name, copytempdir->name))
+			if (!strcmp(h->name, copytempdir->name))
 			{
-			cout<<"THE DIR HAS EXSITED           --FAILED"<<endl;
-			return 0;
+				cout << "THE DIR HAS EXSITED           --FAILED" << endl;
+				return 0;
 			}
-            h = h->nextDir;
-        }
-       
-	
-			disk_empty -= copytempdir->size;    //减去大小
-            d1 = copy_dir(copytempdir);           //递归拷贝
-            d1->nextDir = currentDir->dirPtr;
-            currentDir->dirPtr = d1;
-            d1->preDir = currentDir;
-            currentDir->size += copytempdir->size;
-       
-    }
-    cout << "THE DIR HAS BEEN PASTED              --SUCCEED" << endl;
-    return 1;
+			h = h->nextDir;
+		}
+
+
+		disk_empty -= copytempdir->size;    //减去大小
+		d1 = copy_dir(copytempdir);           //递归拷贝
+		d1->nextDir = currentDir->dirPtr;
+		currentDir->dirPtr = d1;
+		d1->preDir = currentDir;
+		currentDir->size += copytempdir->size;
+
+	}
+	cout << "THE DIR HAS BEEN PASTED              --SUCCEED" << endl;
+	return 1;
 }
